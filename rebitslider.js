a4485f619046c4f161bc63f876c1e51cc8fb1c0e
Davekrush <107316680+Davekrush@users.noreply.github.com>
2023-03-04 19:43:51 +0100

    Create rebitslider.js

---
+++ rebitslider.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+    $('.rebitslider').slick({
+      dots: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 1000,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
+  
